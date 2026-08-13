9620a742f432cda89a7ec745494dba0405aae0a8
= <=>
2026-08-14 02:46:32 +0700

    feat: implement logsheet and refrezing logsheet pages for operator module

--- resources/js/Pages/Operator/Logsheet.jsx
+++ resources/js/Pages/Operator/Logsheet.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { Head, Link, router } from '@inertiajs/react';
 import OperatorLayout from '@/Layouts/OperatorLayout';
 
